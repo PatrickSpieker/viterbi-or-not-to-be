8fd0ecc70153fd93d43dcbc338e746d9c5fa20a6
Aaron Johnston <aaron.johnston001@gmail.com>
2018-04-16 14:09:04 -0700

    Add annotation parsing and preliminary Naive Bayes implementation

--- baseline/parse_xml.py
+++ baseline/parse_xml.py
@@ -3,18 +3,76 @@
 import numpy as np
 import xml.etree.ElementTree as ET
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import GaussianNB
 from functools import reduce
 
 DEBUG = True
+CORPUS = 'baseline/data/corpus-tiny.xml'
+ANNOTATIONS = 'baseline/data/annotations-tiny.xml'
 
 def main():
-    with open('baseline/data/corpus-tiny.xml', 'r') as xml_file:
-        threads = parse_corpus(xml_file)
+    with open(CORPUS, 'r') as corpus_file, open(ANNOTATIONS, 'r') as annotations_file:
+        annotations = parse_annotations(annotations_file)
+        threads, thread_labels = parse_corpus(corpus_file, annotations)
         sentence_features = calculate_features(threads)
+        model = train_model(sentence_features, thread_labels)
 
 def debug(output):
     if DEBUG:
         print(output)
+
+def parse_annotations(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    annotations = {}
+
+    for thread in root:
+        listno = thread.find('listno').text
+
+        # Note: only using the first annotation for now. There are multiple
+        # annotations available for each email thread.
+        annotation = thread.find('annotation')
+        sentence_ids = []
+        for item in annotation.find('sentences'):
+            sentence_ids.append(item.attrib['id'])
+        
+        # Associate the listno with the list of extractive summary sentences
+        annotations[listno] = sentence_ids
+
+    return annotations
+
+def parse_corpus(xml_file, annotations):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    threads = []
+    thread_labels = []
+
+    for thread in root:
+        thread_text = []
+        sentence_labels = []
+        listno = thread[1].text # TODO: use find syntax
+        debug('---------- Thread with name "' + thread[0].text + '" and listno ' + thread[1].text + ' ----------')
+        
+        for docnum in range(2, len(thread.getchildren())):
+            # { Received, From, To, (Cc), Subject, Text }
+            for item in thread[docnum]:
+                if item.tag == 'Subject':
+                    debug('\n    Email subject: "' + thread[docnum][3].text + '"')
+                if item.tag == 'Text':
+                    for sent in item:
+                        debug('        Sentence id: ' + sent.attrib['id'])
+                        sentence_id = sent.attrib['id']
+                        sentence_labels.append(1 if sentence_id in annotations[listno] else 0)
+                        debug('        Sentence: "' + sent.text + '"')
+                        thread_text.append(sent.text)
+
+        debug('\n')
+        threads.append(thread_text)
+        thread_labels.append(sentence_labels)
+
+    return threads, thread_labels
 
 def calculate_features(threads):
     documents = [' '.join(x) for x in threads]
@@ -45,31 +103,17 @@ def calculate_features(threads):
     debug(sentence_features)
     return sentence_features
 
-def parse_corpus(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+def train_model(sentence_features, thread_labels):
+    # Flatten the thread_labels to produce sentence labels
+    sentence_labels = [label for thread in thread_labels for label in thread]
 
-    threads = []
+    debug(sentence_labels)
 
-    for thread in root:
-        thread_text = []
-        debug('---------- Thread with name "' + thread[0].text + '" and listno ' + thread[1].text + ' ----------')
-        
-        for docnum in range(2, len(thread.getchildren())):
-            # { Received, From, To, (Cc), Subject, Text }
-            for item in thread[docnum]:
-                if item.tag == 'Subject':
-                    debug('\n    Email subject: "' + thread[docnum][3].text + '"')
-                if item.tag == 'Text':
-                    for sent in item:
-                        debug('        Sentence id: ' + sent.attrib['id'])
-                        debug('        Sentence: "' + sent.text + '"')
-                        thread_text.append(sent.text)
+    # Train the Naive Bayes model
+    model = GaussianNB()
+    model.fit(sentence_features, sentence_labels)
 
-        threads.append(thread_text)
-        debug('\n')
-
-    return threads
+    return model
 
 if __name__ == '__main__':
     main()

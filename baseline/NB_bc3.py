cb9d8425881158ce74069f949f105bfe7a7efeff
Lynsey Liu <lynseyliu@gmail.com>
2018-04-17 12:27:51 -0700

    Add special terms feature

--- baseline/NB_bc3.py
+++ baseline/NB_bc3.py
@@ -11,6 +11,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics.pairwise import linear_kernel
 from functools import reduce
 from scipy import spatial
+from nltk import tokenize
+from nltk import tag
 
 def main():
     with open(config.DATA_DIR + config.CORPUS + config.TRAIN, 'r') as corpus_file, open(config.DATA_DIR + config.ANNOTATIONS + config.TRAIN, 'r') as annotations_file:
@@ -93,7 +95,7 @@ def calculate_features(threads, thread_names):
     num_of_sentences = reduce(lambda s, thread: s + len(thread), threads, 0)
     global_sentence_index = 0
 
-    sentence_features = np.ndarray(shape=(num_of_sentences, 8))
+    sentence_features = np.ndarray(shape=(num_of_sentences, 9))
 
     for thread_index, thread in enumerate(threads):
 
@@ -104,6 +106,28 @@ def calculate_features(threads, thread_names):
         tf_isf = tf_isf_vectorizer.fit_transform(thread_with_name)
         tf_isf_features = np.squeeze(np.asarray(np.mean(tf_isf, axis=1)))
         title_vector = tf_isf[len(thread_with_name) - 1]
+
+        # Count number of special terms in thread
+        special_counts = []
+        total_special_count = 0.0
+        for sentence in thread:
+            sent_tokens = tokenize.word_tokenize(sentence)
+            tagged_sent = tag.pos_tag(sent_tokens)
+
+            prev_proper_index = 1
+            sent_special_count = 0.0
+
+            for word_index, tagged_word in enumerate(tagged_sent):
+                pos = tagged_word[1]
+                if pos == 'NNP':
+                    if prev_proper_index != word_index - 1:
+                        sent_special_count = sent_special_count + 1.0
+                    prev_proper_index = word_index
+                elif pos == 'CD':
+                    sent_special_count = sent_special_count + 1.0
+            
+            special_counts.append(sent_special_count)
+            total_special_count = total_special_count + sent_special_count
 
         for sentence_index, sentence in enumerate(thread):
             # TF-IDF
@@ -120,10 +144,12 @@ def calculate_features(threads, thread_names):
             # Centroid Coherence
             tf_isf_mean = np.mean(tf_isf, axis=0)
             sentence_features[global_sentence_index, 5] = linear_kernel(tf_isf_mean, sentence_vector).flatten()
+            # Special Terms
+            sentence_features[global_sentence_index, 6] = special_counts[sentence_index] / total_special_count
             # Special Case: Starts with '>'
-            sentence_features[global_sentence_index, 6] = 0 if sentence.startswith('>') else 1
+            sentence_features[global_sentence_index, 7] = 0 if sentence.startswith('>') else 1
             # Position from the end of the email
-            sentence_features[global_sentence_index, 7] = len(thread) - sentence_index
+            sentence_features[global_sentence_index, 8] = len(thread) - sentence_index
 
             global_sentence_index += 1
 
